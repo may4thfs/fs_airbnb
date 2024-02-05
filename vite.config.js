daab949a95735791660ee890763cc4eda39f97bf
fengshun <723278075@qq.com>
2024-02-05 11:58:11 +0800

    feat: 集成了 antd 和 mui 组件库

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // 动态地获取 "src" 目录的路径，并将其映射到 "src" 别名，和直接写 "@": "/src" 结果一样。只是前者更加通用，因为它不依赖于项目的具体布局
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			// 动态地获取 "src" 目录的路径，并将其映射到 "src" 别名，和直接写 "@": "/src" 结果一样。只是前者更加通用，因为它不依赖于项目的具体布局
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			// 别名配置：当代码中导入 @mui/styled-engine 模块时，Vite 实际上会导入 @mui/styled-engine-sc 模块
+			"@mui/styled-engine": "@mui/styled-engine-sc"
+		}
+	}
 })
